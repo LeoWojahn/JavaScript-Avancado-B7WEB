f593446e5a5fb1507be82ca68a5e3bfa214f128f
Leo Wojahn <leowojahn@gmail.com>
2026-08-08 14:55:11 -0300

    feat: manipulação de number

---
+++ 3-manipulacoes-arrow-ajax-etc/3.2-metodos-numero/script.js
@@ -0,0 +1,9 @@
+let n = "25";
+let res = parseInt(n) + 5;
+
+console.log(res);
+
+// toString() transforma algo pra string
+// toFixed() define quantos numeros depois do . e arredonda
+// parseInt() transforma uma string num número
+// parseFloat() transforma uma string num número com casas decimais
