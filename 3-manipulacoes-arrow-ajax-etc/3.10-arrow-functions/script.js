b0e8b6c90cd99cb5c4b88ddea01aa2fa25927217
Leo Wojahn <leowojahn@gmail.com>
2026-08-11 20:02:36 -0300

    feat: arrow functions

---
+++ 3-manipulacoes-arrow-ajax-etc/3.10-arrow-functions/script.js
@@ -0,0 +1,28 @@
+/*
+Função comum.
+
+function somar(x, y) {
+	return x + y;
+}
+
+*/
+
+/*
+Função na variável.
+
+let somar = function(x, y) {
+	return x + y;
+};
+
+*/
+
+/*
+Arrow function comum.
+
+let somar = (x, y) => {
+	return x + y;
+};
+*/
+
+let somar = (x, y) => x + y;
+console.log(somar(10, 5));
