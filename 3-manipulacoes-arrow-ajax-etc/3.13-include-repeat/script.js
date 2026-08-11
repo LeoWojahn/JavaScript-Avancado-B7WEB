c559bc759167e2b65a0c5fea420f74de2eb2891c
Leo Wojahn <leowojahn@gmail.com>
2026-08-11 20:25:40 -0300

    feat: include e repeat

---
+++ 3-manipulacoes-arrow-ajax-etc/3.13-include-repeat/script.js
@@ -0,0 +1,8 @@
+let lista = ["ovo", "café", "arroz", "feijão", "macarrão"];
+
+// O include retorna true ou false dependendo de tiver ou n no array/variável
+console.log(lista.includes("ovo"));
+
+// Simplesmente repete
+nome = "Leonardo";
+console.log(nome.repeat(2));
