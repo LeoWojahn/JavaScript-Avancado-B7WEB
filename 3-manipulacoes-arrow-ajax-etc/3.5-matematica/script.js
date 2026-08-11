e434e2f67c97f84f46e8820202f9659b499e68be
Leo Wojahn <leowojahn@gmail.com>
2026-08-10 21:42:48 -0300

    feat: math e interval

---
+++ 3-manipulacoes-arrow-ajax-etc/3.5-matematica/script.js
@@ -0,0 +1,15 @@
+let novoValor = Math.PI;
+
+console.log(novoValor);
+
+// .round() sempre arredonda pra um inteiro. maior ou igual a 0.5, arredonda pra cima. Se n pra baixo.
+console.log(Math.round(novoValor));
+
+// .floor() sempre arredonda pra baixo
+console.log(Math.floor(3.75));
+
+// .ceil() sempre arredonda pra cima
+console.log(Math.ceil(3.1));
+
+// .random() gera um número aleatório
+console.log(Math.random());
---
+++ 3-manipulacoes-arrow-ajax-etc/3.6-intervalos/script.js
@@ -0,0 +1,29 @@
+// setInterval()
+let timer;
+
+function comecar() {
+    timer = setInterval(showTime, 1000);
+}
+
+function parar() {
+    clearInterval(timer);
+}
+
+function showTime() {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let txt = h+':'+m+':'+s;
+
+    document.querySelector('.demo').innerHTML = txt;
+}
+
+/*
+setTimeOut() 
+Ele roda e depois de um tempo para.
+*/
+
+setTimeout(function() {
+    alert("Rodou!");
+}, 2000);
