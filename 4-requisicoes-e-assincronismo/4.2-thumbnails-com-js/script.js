c768dd3287fe71af909d481889a60f56a2da6e10
Leo Wojahn <leowojahn@gmail.com>
2026-08-11 21:28:08 -0300

    feat: thumbnails com js

---
+++ 4-requisicoes-e-assincronismo/4.2-thumbnails-com-js/script.js
@@ -0,0 +1,10 @@
+function mostrar() {
+	let imagem = document.querySelector("#imagem").files[0];
+
+	let img = document.createElement("img");
+
+	img.src = URL.createObjectURL(imagem);
+	img.width = 200;
+
+	document.querySelector("#area").appendChild(img);
+}
